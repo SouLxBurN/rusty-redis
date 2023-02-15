af45807e1839031491ea9083f9e10d3818c7589e
Tristan Farmer <tristanfarmer@gmail.com>
2023-02-14 22:56:42 -0800

    chore: Server code cleanup

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -10,27 +10,23 @@ pub async fn start_server() {
 
 async fn listen(listener: TcpListener) {
     loop {
-        match listener.accept().await {
-            Ok((stream, _addr)) => {
-                println!("Receiving Incoming Transmission");
-                let mut conn = RedisConnection::new(stream);
-                tokio::spawn(async move {
-                    let mut read_buf = [0u8; BUF_MAX];
-                    loop { // Maintain Connection
-                        if let Ok(n) = conn.read_message(&mut read_buf).await {
-                            println!("{}", str::from_utf8(&read_buf[0..n]).unwrap());
-                            if let Err(e) = conn.write_message("Hi Client! I'm Dad!".as_bytes()).await {
-                                eprintln!("Failed to write message {}", e);
-                                break;
-                            }
-                        } else {
-                            println!("read_full ended");
-                            break;
-                        }
+        let (stream, _addr) = listener.accept().await.expect("Failed to accept connection");
+        println!("Receiving Incoming Transmission");
+        let mut conn = RedisConnection::new(stream);
+        tokio::spawn(async move {
+            let mut read_buf = [0u8; BUF_MAX];
+            loop {
+                if let Ok(n) = conn.read_message(&mut read_buf).await {
+                    println!("{}", str::from_utf8(&read_buf[0..n]).unwrap());
+                    if let Err(e) = conn.write_message("Hi Client! I'm Dad!".as_bytes()).await {
+                        eprintln!("Failed to write message {}", e);
+                        break;
                     }
-                });
-            },
-            Err(e) => panic!("Failed to accept connection: {}", e),
-        }
+                } else {
+                    println!("connection ended");
+                    break;
+                }
+            }
+        });
     }
 }
